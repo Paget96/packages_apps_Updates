b7f254d125614a6cc851ea0f478b8b331fa2527f
Joey Huab <joey@evolution-x.org>
2020-03-09 07:51:31 +0000

    Updater: Add Evolution X version under Android version

--- src/org/evolution/ota/UpdatesActivity.java
+++ src/org/evolution/ota/UpdatesActivity.java
@@ -27,6 +27,7 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.SystemProperties;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -84,6 +85,7 @@ public class UpdatesActivity extends UpdatesListActivity {
     private Button checkUpdateButton;
     private TextView updateStatus;
     private TextView androidVersion;
+    private TextView evolutionVersion;
     private TextView securityVersion;
     private TextView lastUpdateCheck;
     private String LastUpdateCheck;
@@ -121,12 +123,15 @@ public class UpdatesActivity extends UpdatesListActivity {
         checkUpdateButton = findViewById(R.id.check_updates);
         updateStatus = findViewById(R.id.no_new_updates_view);
         androidVersion = findViewById(R.id.android_version);
+        evolutionVersion = findViewById(R.id.evolution_version);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         lastUpdateCheck = findViewById(R.id.last_update_check);
         securityVersion = findViewById(R.id.security_version);
 
         androidVersion.setText(String.format(getResources()
                 .getString(R.string.android_version, Build.VERSION.RELEASE)));
+        evolutionVersion.setText(String.format(getResources()
+                .getString(R.string.evolution_version, SystemProperties.get("org.evolution.build_version"))));
         securityVersion.setText(String.format(getResources()
                 .getString(R.string.security_patch_level), Utils.getSecurityPatchLevel()));
         lastUpdateCheck.setText(String.format(getResources()
@@ -256,6 +261,7 @@ public class UpdatesActivity extends UpdatesListActivity {
 
         findViewById(R.id.recycler_view).setVisibility(View.GONE);
         androidVersion.setVisibility(View.VISIBLE);
+        evolutionVersion.setVisibility(View.VISIBLE);
         securityVersion.setVisibility(View.VISIBLE);
         lastUpdateCheck.setVisibility(View.VISIBLE);
     }
@@ -267,6 +273,7 @@ public class UpdatesActivity extends UpdatesListActivity {
 
         findViewById(R.id.recycler_view).setVisibility(View.VISIBLE);
         androidVersion.setVisibility(View.GONE);
+        evolutionVersion.setVisibility(View.GONE);
         securityVersion.setVisibility(View.GONE);
         lastUpdateCheck.setVisibility(View.GONE);
 
@@ -424,6 +431,7 @@ public class UpdatesActivity extends UpdatesListActivity {
         securityVersion.setVisibility(View.GONE);
         lastUpdateCheck.setVisibility(View.GONE);
         androidVersion.setVisibility(View.GONE);
+        evolutionVersion.setVisibility(View.GONE);
 
         if (mRefreshIconView == null) {
             mRefreshIconView = findViewById(R.id.menu_refresh);
